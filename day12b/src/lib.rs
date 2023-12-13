c6590f310032ca41ff7a7343e92c9fd555acf632
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2023-12-13 09:33:33 +0300

    add solution for day12b.

--- day12b/src/lib.rs
+++ day12b/src/lib.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+type ReturnType = u128;
+
 #[derive(Debug, Eq, PartialEq, Clone, Copy)]
 enum Status {
     Unknown,
@@ -38,15 +42,20 @@ impl TryFrom<&str> for Line {
             .split(',')
             .map(|n| n.parse::<usize>().map_err(|_err| "Cannot parse value"))
             .collect::<Result<Vec<usize>, Self::Error>>()?;
-        let mut unfolded_statuses= Vec::new();
+        let mut unfolded_statuses = Vec::new();
         let mut unfolded_groups = Vec::new();
 
-        for _i in 0..5 {
+        for i in 0..5 {
             unfolded_statuses.extend(&statuses);
-            unfolded_statuses.push(Status::Unknown);
+            if i != 4 {
+                unfolded_statuses.push(Status::Unknown);
+            }
             unfolded_groups.extend(&groups);
         }
-        Ok(Line { statuses: unfolded_statuses, groups: unfolded_groups })
+        Ok(Line {
+            statuses: unfolded_statuses,
+            groups: unfolded_groups,
+        })
     }
 }
 
@@ -93,87 +102,64 @@ impl TryFrom<&str> for Input {
     }
 }
 
-fn get_combs(count: usize) -> Vec<Vec<Status>> {
-    if count == 0 {
-        return vec![Vec::new()];
-    } else {
-        let mut result = Vec::new();
-        let rest = get_combs(count - 1);
-        for mut r in rest {
-            let mut a = vec![Status::Working];
-            let mut b = vec![Status::Damaged];
-            a.extend(&r);
-            b.append(&mut r);
-            result.push(a);
-            result.push(b);
-        }
-        result
-    }
-}
-
-fn merge_comb(statuses: &[Status], comb: Vec<Status>) -> Vec<Status> {
-    let mut result = Vec::new();
-    let mut comb_it = comb.into_iter();
-    for s in statuses {
-        if *s == Status::Unknown {
-            result.push(comb_it.next().unwrap())
+fn count_matches(
+    statuses: &[Status],
+    groups: &[usize],
+    cache: &mut HashMap<String, ReturnType>,
+) -> ReturnType {
+    //dbg!(statuses, groups);
+    // is status is empty finish recursion.
+    if statuses.is_empty() {
+        if groups.is_empty() {
+            return 1;
         } else {
-            result.push(s.clone());
+            return 0;
         }
     }
+
+    //If value is in cache return it from cache.
+    let cache_key = format!("{}-{}", statuses.len(), groups.len());
+    if cache.contains_key(&cache_key) {
+        return *cache.get(&cache_key).unwrap();
+    }
+
+    let mut result = 0;
+
+    // Case where first element is not part of the first group
+    if [Status::Working, Status::Unknown].contains(&statuses[0]) {
+        result += count_matches(&statuses[1..], groups, cache);
+    }
+
+    //Case where first element is part of the group
+    if !groups.is_empty() {
+        let group = groups[0];
+        if statuses.len() >= group
+            && statuses[..group]
+                .iter()
+                .all(|s| [Status::Damaged, Status::Unknown].contains(s))
+        {
+            if statuses.len() == group {
+                result += count_matches(&statuses[group..], &groups[1..], cache);
+            } else if statuses.len() > group
+                && [Status::Working, Status::Unknown].contains(&statuses[group])
+            {
+                // Next element after the group is valid
+                // Do not send the next element to recursive call so we are removing it with the group.
+                result += count_matches(&statuses[group + 1..], &groups[1..], cache);
+            }
+        }
+    }
+
+    cache.insert(cache_key, result);
     result
 }
 
-fn is_match(statuses: &[Status], groups: &[usize]) -> bool {
-    let mut counts: Vec<usize> = Vec::new();
-    let mut current_count = 0;
-    for s in statuses {
-        if *s == Status::Working {
-            if current_count > 0 {
-                counts.push(current_count);
-                current_count = 0;
-            }
-        } else {
-            current_count += 1;
-        }
-    }
-    if current_count > 0 {
-        counts.push(current_count);
-    }
-    groups.len() == (&counts).len() && groups.iter().zip(counts).all(|(a,b)|*a==b)
-}
-
-fn match_count(statuses: &[Status], groups: &[usize]) -> u128 {
-    let unknown_count = statuses
-        .iter()
-        .map(|s| if *s == Status::Unknown { 1 } else { 0 })
-        .sum();
-    println!("getting unknown combs");
-    let unknown_combs = get_combs(unknown_count);
-    println!("getting unknown combs...done");
-    // let combs: Vec<Vec<Status>> = unknown_combs
-    //     .into_iter()
-    //     .map(|u| merge_comb(statuses, u))
-    //     .collect();
-    //
-    // //dbg!(statuses, groups, unknown_count, combs);
-    // combs
-    //     .into_iter()
-    //     .map(|s| if is_match(&s, groups) { 1 } else { 0 })
-    //     .sum()
-
-    unknown_combs
-        .into_iter()
-        .map(|u| merge_comb(statuses, u))
-        .map(|s| if is_match(&s, groups) { 1 } else { 0 })
-        .sum()
-}
-fn get_match_count(line: &Line) -> u128 {
-    let result = match_count(&line.statuses, &line.groups);
+fn get_match_count(line: &Line) -> ReturnType {
+    let result = count_matches(&line.statuses, &line.groups, &mut HashMap::new());
     // dbg!(&line, &result);
     result
 }
-fn calculate(input: &mut Input) -> u128 {
+fn calculate(input: &mut Input) -> ReturnType {
     println!("{}", input.to_string());
     input.lines.iter().map(get_match_count).sum()
 }
@@ -189,64 +175,64 @@ pub fn run() {
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let sample_input = "
-???.### 1,1,3
-.??..??...?##. 1,1,3
-?#?#?#?#?#?#?#? 1,3,1,6
-????.#...#... 4,1,1
-????.######..#####. 1,6,5
-?###???????? 3,2,1
-";
-        let result = calculate(&mut Input::try_from(sample_input).unwrap());
-        assert_eq!(result, 525152);
-    }
-
-    #[test]
-    fn it_works2() {
-        let sample_input = "
-???.### 1,1,3
-";
-        let result = calculate(&mut Input::try_from(sample_input).unwrap());
-        assert_eq!(result, 1);
-    }
-
-    #[test]
-    fn it_works3() {
-        let sample_input = "
-.??..??...?##. 1,1,3
-";
-        let result = calculate(&mut Input::try_from(sample_input).unwrap());
-        assert_eq!(result, 16384);
-    }
-
-    #[test]
-    fn it_works4() {
-        let sample_input = "
-?#?#?#?#?#?#?#? 1,3,1,6
-";
-        let result = calculate(&mut Input::try_from(sample_input).unwrap());
-        assert_eq!(result, 1);
-    }
-
-    #[test]
-    fn it_works5() {
-        let sample_input = "
-????.#...#... 4,1,1
-";
-        let result = calculate(&mut Input::try_from(sample_input).unwrap());
-        assert_eq!(result, 16);
-    }
-
-    #[test]
-    fn it_works6() {
-        let sample_input = "
-????.######..#####. 1,6,5
-";
-        let result = calculate(&mut Input::try_from(sample_input).unwrap());
-        assert_eq!(result, 2500);
-    }
+    //     #[test]
+    //     fn it_works() {
+    //         let sample_input = "
+    // ???.### 1,1,3
+    // .??..??...?##. 1,1,3
+    // ?#?#?#?#?#?#?#? 1,3,1,6
+    // ????.#...#... 4,1,1
+    // ????.######..#####. 1,6,5
+    // ?###???????? 3,2,1
+    // ";
+    //         let result = calculate(&mut Input::try_from(sample_input).unwrap());
+    //         assert_eq!(result, 525152);
+    //     }
+    //
+    //     #[test]
+    //     fn it_works2() {
+    //         let sample_input = "
+    // ???.### 1,1,3
+    // ";
+    //         let result = calculate(&mut Input::try_from(sample_input).unwrap());
+    //         assert_eq!(result, 1);
+    //     }
+    //
+    //     #[test]
+    //     fn it_works3() {
+    //         let sample_input = "
+    // .??..??...?##. 1,1,3
+    // ";
+    //         let result = calculate(&mut Input::try_from(sample_input).unwrap());
+    //         assert_eq!(result, 16384);
+    //     }
+    //
+    //     #[test]
+    //     fn it_works4() {
+    //         let sample_input = "
+    // ?#?#?#?#?#?#?#? 1,3,1,6
+    // ";
+    //         let result = calculate(&mut Input::try_from(sample_input).unwrap());
+    //         assert_eq!(result, 1);
+    //     }
+    //
+    //     #[test]
+    //     fn it_works5() {
+    //         let sample_input = "
+    // ????.#...#... 4,1,1
+    // ";
+    //         let result = calculate(&mut Input::try_from(sample_input).unwrap());
+    //         assert_eq!(result, 16);
+    //     }
+    //
+    //     #[test]
+    //     fn it_works6() {
+    //         let sample_input = "
+    // ????.######..#####. 1,6,5
+    // ";
+    //         let result = calculate(&mut Input::try_from(sample_input).unwrap());
+    //         assert_eq!(result, 2500);
+    //     }
 
     #[test]
     fn it_works7() {
@@ -257,4 +243,34 @@ mod tests {
         assert_eq!(result, 506250);
     }
 
+//     #[test]
+//     fn input_parsing_test() {
+//         let sample_input = "
+// .# 1
+// ";
+//         let input = Input::try_from(sample_input).unwrap();
+//         println!("{:?}", input);
+//         assert_eq!(
+//             input.lines,
+//             vec![Line {
+//                 statuses: vec![
+//                     Status::Working,
+//                     Status::Damaged,
+//                     Status::Unknown,
+//                     Status::Working,
+//                     Status::Damaged,
+//                     Status::Unknown,
+//                     Status::Working,
+//                     Status::Damaged,
+//                     Status::Unknown,
+//                     Status::Working,
+//                     Status::Damaged,
+//                     Status::Unknown,
+//                     Status::Working,
+//                     Status::Damaged
+//                 ],
+//                 groups: vec![1, 1, 1, 1, 1]
+//             }]
+//         );
+//     }
 }
